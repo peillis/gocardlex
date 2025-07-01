e6f66cb9e6b1477bfbc36ee32d8f7b3085584cf1
Chema Martínez Manzanares <j.martinez@doofinder.com>
2025-07-01 10:12:53 +0200

    Missing file

---
+++ lib/gocardlex/api/billing_request.ex
@@ -0,0 +1,27 @@
+defmodule Gocardlex.Api.BillingRequest do
+  import Gocardlex.Utils.Comms
+
+  def create(body) do
+    request(:post, "/billing_requests", body)
+  end
+
+  def collect_customer_details(billing_request_id, body) do
+    request(:post, "/billing_requests/#{billing_request_id}/actions/collect_customer_details", body)
+  end
+
+  def collect_bank_account_details(billing_request_id, body) do
+    request(:post, "/billing_requests/#{billing_request_id}/actions/collect_bank_account_details", body)
+  end
+
+  def confirm_payer_details(billing_request_id, body) do
+    request(:post, "/billing_requests/#{billing_request_id}/actions/confirm_payer_details", body)
+  end
+
+  def fulfill(billing_request_id, body) do
+    request(:post, "/billing_requests/#{billing_request_id}/actions/fulfill", body)
+  end
+
+  def cancel(billing_request_id, body) do
+    request(:post, "/billing_requests/#{billing_request_id}/actions/cancel", body)
+  end
+end
