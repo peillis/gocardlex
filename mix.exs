a9b8b0a90e9b9590f62b6518524176a1413acaae
peillis <enrique@doofinder.com>
2026-09-21 11:39:17 +0200

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gocardlex.Mixfile do
   def project do
     [
       app: :gocardlex,
-      version: "1.1.0",
+      version: "2.0.0-rc.1",
       package: package(),
       elixir: "~> 1.18",
       build_embedded: Mix.env() == :prod,
